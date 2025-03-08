957e81d926bb73633933f9aec4b2c251050d99bb
Anurag <anuragsonkar053@gmail.com>
2025-03-08 18:17:54 +0530

    use-import

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@
 //   plugins: [],
 // }
 
-const withMT = require("@material-tailwind/react/utils/withMT");
+// const withMT = require("@material-tailwind/react/utils/withMT");
+import withMT from '@material-tailwind/react/utils/withMT';
 
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
